f43c3506eb3f3668d67e1b883fc68f6df54e0607
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-01-11 10:21:15 -0500

    add file

---
+++ lab_3-1.py
@@ -0,0 +1,15 @@
+# Author MB 01/11/2022
+def add_foods(food_list):
+
+    sixth_varible = []
+    not_food = []
+    short_foods = []
+
+        for food in food_list:
+
+
+
+add_foods(['potatoes', 'salsa', 'cherries', 'banana', 'apple'])
+add_foods(['naan', 'celery', 'buckwheat', 7, 'clementine'])
+add_foods(['cheeseburger', True, 'chicken', 'rice', 'radish'])
+        
